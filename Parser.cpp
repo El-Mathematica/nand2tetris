56c990c6935eafcb1564409bb443e47de5b8e61a
El-Mathematica <menongokuls@gmail.com>
2022-06-27 22:28:18 +0100

    developed hasMoreCommands() function

--- Parser.cpp
+++ Parser.cpp
@@ -1,16 +1,27 @@
 #include "Parser.h"
 #include <iostream>
+#include <limits>
 
 using namespace std;
 
 Parser::Parser(string filename) {
         file.open(filename);
         for(string line; getline(file, line); ) {
-        cout << line << endl;
+            cout << boolalpha << hasMoreCommands();
+            cout << line << endl;
     }
 }
 
 bool Parser::hasMoreCommands() {
-    cout << "running hasMoreCommands" << endl;
-    return true;
+    int c = file.peek();
+    if (c == EOF) {
+        if (file.eof()) {
+            return 0;
+        }
+        else {
+        // error
+            return 1;
+        }
+        return 1;
+    }
 }
